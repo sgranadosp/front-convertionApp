b5a89af48865eea38d91efd3fb516a24d9aca275
sgranadosp <sgranadosp@unbosque.edu.co>
2025-05-19 00:11:01 -0500

    Título ConvImages

--- src/pages/ConvImages.tsx
+++ src/pages/ConvImages.tsx
@@ -3,12 +3,25 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import FileUploaderImg from "../components/FileUploaderImg";
 import PrimeReactWrapper from "../PrimeReactWrapper";
+import "./ConvImages.css";
 
 const ConvImages = () => {
   return (
     <>
       <div>
         <NavBar />
+      </div>
+
+      <div
+        style={{
+          paddingTop: "1rem",
+        }}
+      >
+        <h1 className="title-text">Conversión de Imágenes</h1>
+        <h5 className="subtitle-text">
+          ¡Aquí puedes convertir tus imágenes a cualquier formato que puedas
+          imaginar!
+        </h5>
       </div>
 
       <div style={{ padding: "5rem" }}>
