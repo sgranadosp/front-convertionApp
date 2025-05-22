b77008562c7c602488e9772da6697cf9233e42bc
sgranadosp <sgranadosp@unbosque.edu.co>
2025-05-21 23:13:16 -0500

    arregle error de import de css inexistente

--- src/pages/AfterLogIn.tsx
+++ src/pages/AfterLogIn.tsx
@@ -25,7 +25,6 @@
 import React from "react";
 import AfterLoginForm from "../components/AfterLoginForm";
 import NavBar from "../components/NavBar";
-import "./AfterLogin.css";
 
 const AfterLogIn = () => {
   return (
