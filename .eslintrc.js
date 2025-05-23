c2004e566cf31c21590a683c882744d274f466a0
sgranadosp <sgranadosp@unbosque.edu.co>
2025-05-23 03:50:55 -0500

    deepsource

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // Personaliza reglas aquí si lo deseas
+    'react/react-in-jsx-scope': 'off', // No es necesario con Vite y React 17+
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+};
